cb14446e15c3b0963a42bf39f99277ed590ae58a
Farouk <faroukkisuule55@gmail.com>
2022-02-22 19:49:51 -0500

    Project now set up with navbar component added to top of the page

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+   return (
+      <div>
+         <Navbar />
+         {/* <Home /> */}
+      </div>
+   );
+};
+
+export default App;
+
+// Installing new caniuse-lite version
+// $ npm install caniuse-lite
+// Cleaning package.json dependencies from caniuse-lite
+// $ npm uninstall caniuse-lite
+// caniuse-lite has been successfully updated
