779c5a3a8f2c4df033c97622687a7731c57b1787
Farouk <faroukkisuule55@gmail.com>
2022-02-23 07:45:51 -0500

    Added category component with different items

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
-import Announcement from "./components/Announcement";
-import Navbar from "./components/Navbar";
-import Slider from "./components/Slider";
 import Home from "./pages/Home";
 
 const App = () => {
    return (
       <div>
-         <Announcement />
-         <Navbar />
-         <Slider/>
+       <Home/>
       </div>
    );
 };
