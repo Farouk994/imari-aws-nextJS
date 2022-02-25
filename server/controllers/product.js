014e9bd179af98657d5bde5fad2203effc157e48
Farouk <faroukkisuule55@gmail.com>
2022-02-25 13:24:07 -0500

    Product controller has been create for user or admin to carry out CRUD functions

---
+++ server/controllers/product.js
@@ -0,0 +1,73 @@
+const Product = require("../models/Product");
+
+// Add Product
+const addProduct = async (req, res) => {
+   const product = new Product(req.body);
+   try {
+      const savedProduct = await product.save();
+      res.status(200).json(savedProduct);
+   } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error");
+   }
+};
+
+// Get ProductById
+const getProductById = async (req, res) => {
+   try {
+      const product = await Product.findById(req.params.id);
+      res.status(200).json(product);
+   } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+   }
+};
+
+// Get All Products
+const getAllProducts = async (req, res) => {
+   try {
+      const product = await Product.find();
+      res.status(200).json(product);
+   } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+   }
+};
+
+// Update Product
+const updateProduct = async (req, res) => {
+   try {
+      const product = await Product.findByIdAndUpdate(
+         req.params.id,
+         {
+            $set: req.body,
+         },
+         { new: true }
+      );
+      if (!product) return res.status(400).send("Item wasn't found, try again");
+      res.status(200).json(product);
+   } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+   }
+};
+
+// Delete Product
+const deleteProduct = async (req, res) => {
+   try {
+      const product = await Product.findOneAndDelete(req.params.id);
+      if (!product) return res.status(400).send("Product not Found, try Again");
+      res.status(200).send("Product has now been Deleted");
+   } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+   }
+};
+
+module.exports = {
+   addProduct,
+   deleteProduct,
+   updateProduct,
+   getAllProducts,
+   getProductById,
+};
