f3539e863d74401356715f8213578651110d8871
Farouk <faroukkisuule55@gmail.com>
2022-02-25 08:08:50 -0500

    Added models to define different schemas to enable routing in the backend with the database

---
+++ server/models/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+   {
+      title: {
+         type: String,
+         required: true,
+      },
+      desc: {
+         type: String,
+         required: true,
+      },
+      categories: {
+         type: Array,
+         required: true,
+      },
+      size: {
+         type: String,
+      },
+      color: {
+         type: String,
+      },
+      price: {
+         type: Number,
+         required: true,
+      },
+      image: {
+         type: String,
+         require: true,
+      },
+   },
+   { timestamps: true }
+);
+
+const Product = mongoose.model("User", productSchema);
+
+module.exports = Product;
